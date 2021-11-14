3671e778d8f5f86e99a57d825770539e84b62d7d
Francis Osei Annin <48407920+fransay@users.noreply.github.com>
2021-11-14 13:59:31 +0000

    Update FrancisOsei.py

--- Palindrome/FrancisOsei.py
+++ Palindrome/FrancisOsei.py
@@ -1,1 +1,11 @@
 
+# string palindrome
+
+def stringPalindrome(string):
+    """check whether string is palindrome or not"""
+    if string == string[::-1]:
+        return f'{string} is a palindrome'
+    return f'{string}is not a palindrome'
+
+
+print(stringPalindrome('madam'))
