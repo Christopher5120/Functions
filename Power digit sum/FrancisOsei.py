7f47f5fef8163736b4c6820d33f5cd7d65bcebfa
Francis Osei Annin <48407920+fransay@users.noreply.github.com>
2021-11-16 10:23:03 +0000

    Update FrancisOsei.py

--- Power digit sum/FrancisOsei.py
+++ Power digit sum/FrancisOsei.py
@@ -1,1 +1,12 @@
+def powerDigitSum(power):
+    sum = 0
+    result_power = pow(2, power)
+    for x in str(result_power):
+        sum += int(x)
+    return sum
+
+
+if __name__ == '__main__':
+    print(powerDigitSum(15))  # This is the output of 26
+    print(powerDigitSum(1000))  # Output for 1000
 
